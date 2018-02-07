7456a53d829c0a262dcd64902d7c5ee6ce32a339
Robin Maisano <robin.maisano@gmail.com>
2018-02-07 23:41:58 +0100

    Thresholding Function

--- Mission1/Mission3.py
+++ Mission1/Mission3.py
@@ -6,7 +6,7 @@ image = cv2.imread("img\Europa_surface.pbm", 0)
 
 def mission3taux(img):
     imgSize = img.shape
-    taux = 220
+    taux = 150
     for i in range(0, imgSize[0]):
         for j in range(0, imgSize[1]):
             if img[i][j] < taux:
@@ -19,9 +19,12 @@ def mission3Thresholding(img):
     imgSize = img.shape
 
 #    img = cv2.medianBlur(img, 3) # Floutage de l'image
-    cv2.imshow('image', img)
+#    cv2.imshow('image', img)
     cv2.waitKey(0)
-  #  th = cv2.adaptativeThreshold(img)
+    th = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 30)
+
+    cv2.imshow('Image', th)
+    cv2.waitKey(0)
 
 #mission3taux(image)
 mission3Thresholding(image)
